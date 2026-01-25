06b45d00c4df6f3b1261dc3184ac1f94bbd00a9a
ThierGauX <lestimegauthier@gmail.com>
2026-01-25 20:24:32 +0100

    Refactor game loop and drawing code

--- avion.py
+++ avion.py
@@ -1,27 +1,26 @@
 import pygame
 
-# 1. On prépare le jeu
+
 pygame.init()
 fenetre = pygame.display.set_mode((600, 400))
-avion = pygame.Rect(100, 200, 50, 30) # Un rectangle (x, y, largeur, hauteur)
+avion = pygame.Rect(100, 200, 50, 30) 
 
-# 2. La boucle du jeu (le moteur)
+
 while True:
-    # On regarde si l'utilisateur veut fermer la fenêtre
+    
     if pygame.event.peek(pygame.QUIT):
         break
 
-    # On regarde sur quelles touches on appuie
     touches = pygame.key.get_pressed()
     if touches[pygame.K_RIGHT]: avion.x += 5
     if touches[pygame.K_LEFT]:  avion.x -= 5
     if touches[pygame.K_UP]:    avion.y -= 5
     if touches[pygame.K_DOWN]:  avion.y += 5
 
-    # On dessine tout
-    fenetre.fill("skyblue")               # On peint le fond en bleu
-    pygame.draw.rect(fenetre, "red", avion) # On dessine l'avion en rouge
-    pygame.display.flip()                 # On montre le dessin à l'écran
+    
+    fenetre.fill("skyblue")              
+    pygame.draw.rect(fenetre, "red", avion)
+    pygame.display.flip()                 
 
-    # On attend un tout petit peu (60 images par seconde)
+    
     pygame.time.Clock().tick(60)
